3767ef658732fc4304d2ec0296b12b5db84330ba
Mikołaj Gaweł <mikolaj.gawel@gmail.com>
2025-04-15 18:05:54 +0200

    Create floyd_warshall.py

---
+++ graphs./floyd_warshall.py
@@ -0,0 +1,17 @@
+#shortest paths from all v to all v O(V^3)
+
+def floyd_warshall(G):
+    n=len(G)
+    dist=[[G[i][j] for j in range(n)] for i in range(n)]
+    parent=[[None for j in range(n)] for i in range(n)]
+
+    for i in range(n):
+        for j in range(n):
+            if G[i][j]!=float("inf"):parent[i][j]=i
+
+    for t in range(0,n):
+        for u in range(n):
+            for v in range(n):
+                dist[u][v]=min(dist[u][v],dist[u][t]+dist[t][v])
+
+    return dist
